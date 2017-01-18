3dcc2aa6e0a0853636b16a416a387abbf255ed29
duanyifei <1821367759@qq.com>
2017-01-18 19:57:07 +0800

    测试接口类

---
+++ FootballClass.java
@@ -0,0 +1,34 @@
+public class FootballClass implements FootballInterFace{
+    private String homeName;
+    private String VisitingName;
+    private int hTS;
+    private int vTS;
+    private int eQ;
+
+    public void setHomeTeam(String name){
+        this.homeName = name;
+    }
+
+    public void setVisitingTeam(String name){
+        this.VisitingName = name;
+    }
+    
+    public void homeTeamScored(int points){
+        this.hTS = points;
+    }
+
+    public void VisitingTeamScored(int points){
+        this.vTS = points;
+    }
+
+    public void endOfQuarter(int quarter){
+        this.eQ = quarter;
+    }
+
+    public static void main(String [] args){
+        FootballClass Ft = new FootballClass();
+        Ft.setHomeTeam("sun");
+        System.out.println("homeName: " + Ft.homeName);
+    }
+}
+
---
+++ FootballInterFace.java
@@ -0,0 +1,5 @@
+public interface FootballInterFace extends SportsInterFace{
+    public void homeTeamScored(int points);
+    public void VisitingTeamScored(int points);
+    public void endOfQuarter(int points);
+}
